a4ed49df5b7ccc9b8684e2a2ea47fa8c9570b4f0
Helen Hastings <hhastings@nw0542.nerdwallet.com>
2015-06-16 17:05:06 -0700

    finished challenge 11

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,28 +1,6 @@
-var calls = 0;
-output = [];
-function callback (err, index, data) {
-  if (err) return console.error(err);
-  output[index] = data;
-  if (++calls == 3) {
-    for (var i = 0; i < 3; i++) {
-      console.log(output[i]);
-    }
-  }
-}
-
-var http = require('http');
-[0,1,2].forEach(function(i) {
-  http.get(process.argv[i + 2], function (response) {
-      response.setEncoding('utf8');
-      var contents = "";
-      response.on('data', function(data) {
-          contents += data;
-      });
-      response.on('error', function(err) {
-          return callback(err);
-      });
-      response.on('end', function() {
-        callback(null, i, contents);
-      })
-  });
-});
+var net = require('net')
+var strftime = require('strftime')
+var server = net.createServer(function (socket) {
+  socket.end(strftime('%F %H:%M') + "\n")
+})
+server.listen(process.argv[2])
