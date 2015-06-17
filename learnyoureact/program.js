fe98060842b0b970394b736f06a08f50c849cdb5
Helen Hastings <hhas77@stanford.edu>
2015-06-17 11:16:09 -0700

    started react and finished first two challenges

---
+++ learnyoureact/program.js
@@ -0,0 +1,17 @@
+var path = require('path')
+
+var express = require('express');
+var app = express();
+
+app.set('port', process.argv[2] || 3000);
+app.set('view engine', 'jsx');
+app.set('views', path.join(__dirname, '/views'));
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+app.use('/', function(req, res) {
+  res.render('index', '');
+});
+
+app.listen(app.get('port'), function() {});
