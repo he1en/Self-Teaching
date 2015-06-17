983c8bfbe2bc50c0f7f1f04ed31777c79bc8f131
Helen Hastings <hhas77@stanford.edu>
2015-06-16 17:07:11 -0700

    added 11

---
+++ learnyounode/ch11.js
@@ -0,0 +1,8 @@
+var net = require('http')
+var fs = require('fs')
+
+var server = net.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain'})
+  fs.createReadStream(process.argv[3]).pipe(res)
+})
+server.listen(process.argv[2])
